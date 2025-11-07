2a72420486d3f49297b396d474e8fb6fbf504e69
kutman <smanovkutman0258@gmail.com>
2025-11-07 12:57:07 +0600

    updated project dockerizing and auth-service liquibase initial roles data

---
+++ auth-service/database/release-1.0.0/data/geoinfo-user-roles-data.sql
@@ -0,0 +1,17 @@
+--liquibase formatted sql
+
+--changeSet daivanov:users-role-data-01
+INSERT INTO sso.user_roles(user_id, role_id)
+SELECT user_id, (SELECT role_id FROM sso.roles WHERE role_code = 'ROLE_ADMIN')
+FROM sso.users
+ON CONFLICT DO NOTHING;
+
+INSERT INTO sso.user_roles(user_id, role_id)
+SELECT user_id, (SELECT role_id FROM sso.roles WHERE role_code = 'ROLE_EDITOR')
+FROM sso.users
+ON CONFLICT DO NOTHING;
+
+INSERT INTO sso.user_roles(user_id, role_id)
+SELECT user_id, (SELECT role_id FROM sso.roles WHERE role_code = 'ROLE_VIEWER')
+FROM sso.users
+ON CONFLICT DO NOTHING;
