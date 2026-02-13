07391cbdc3933654d19f893e9510dd95d2f62035
Kutman Smanov <smanovkutman0258@gmail.com>
2026-02-13 11:48:24 +0600

    updated environments and removed auth-client(integrated inside auth-service)

--- auth-client/src/global/mixins/mixins-lib-plugin.js
+++
@@ -1,9 +0,0 @@
-import DateHelperMixin from './date-helper-mixin'
-import NotificationMixin from './notification-mixin';
-
-export default {
-    install(Vue, opt) {
-        Vue.mixin(DateHelperMixin);
-        Vue.mixin(NotificationMixin);
-    }
-}
--- auth-client/src/views/sign-view/reset-password/forms/constants.js
+++
@@ -1,8 +0,0 @@
-export const STEPS = {
-    INIT: 0,
-    CONFIRM: 1,
-    SUCCESS: 2,
-    PASSWORD: 3,
-    SUCCESS_CHANGE: 4,
-    ERROR_CHANGE: 5
-};
